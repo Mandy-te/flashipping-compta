d13a73804ee672834e2a357150756d2c18b520ec
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2026-09-04 06:50:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
    ========================================================================= */
 
 /* -------- A CONFIGURER ------------------------------------------------- */
-const API_URL = 'https://script.google.com/macros/library/d/1nyn0E9cDpuER_sTDlzti10N-1TNLrj52TV80mylCeBce0pw_it8lEcD7/10';
+const API_URL = 'https://script.google.com/macros/library/d/1nyn0E9cDpuER_sTDlzti10N-1TNLrj52TV80mylCeBce0pw_it8lEcD7/11';
 // Doit etre IDENTIQUE au TOKEN en haut de Code.gs
 const TOKEN   = 'FLASHIPPING@2026;
 /* ----------------------------------------------------------------------- */
