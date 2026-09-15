6fd66b30c03802cf1be7647b6c1a4670e5e22f2f
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2026-09-15 17:33:24 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Flashipping - Service Worker
    Met en cache la coquille de l'app pour un fonctionnement hors ligne. */
 
-const CACHE = 'flashipping-v21';
+const CACHE = 'flashipping-v22';
 const FICHIERS = [
   './',
   './index.html',
